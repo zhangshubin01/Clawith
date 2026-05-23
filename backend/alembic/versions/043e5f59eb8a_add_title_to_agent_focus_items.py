efdd57fbb0dd8916047f8d78ae2abd5ed6345921
QinRui <wisdomspace@hotmail.com>
2026-05-23 18:35:58 +0800

    fix(alembic): adjust migration down_revision to merge_heads_20260521

--- backend/alembic/versions/043e5f59eb8a_add_title_to_agent_focus_items.py
+++ backend/alembic/versions/043e5f59eb8a_add_title_to_agent_focus_items.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '043e5f59eb8a'
-down_revision: Union[str, None] = 'add_user_tenant_onboarding'
+down_revision: Union[str, None] = 'merge_heads_20260521'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
